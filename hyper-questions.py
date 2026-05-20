4d248a3ebb76d47bedd7e3769a6da4719e4ad09e
Mazen Ibrahim <mazen.ibrahim@stud.tu-darmstadt.de>
2026-05-20 16:17:51 +0200

    Added Random Sampling for the hyper questions

--- hyper-questions.py
+++ hyper-questions.py
@@ -1,19 +1,21 @@
 import pandas as pd
-
+import random
+import json
 from pathlib import Path
 
-data_sets = ['Chinese', 'English', 'IT', 'Medicine', 'Pokemon', 'Science']
+data_sets = ["Chinese", "English", "IT", "Medicine", "Pokemon", "Science"]
 
 
-def HyperMajorityVote(k):
+def HyperMajorityVote(k, m):
     hyper_questions = {}
     majority_vote_all_answers = {}
     majority_vote = {}
 
     for data_set in data_sets:
-        data_set_path = Path("datasets") / data_set / 'answer.csv'
+        data_set_path = Path("datasets") / data_set / "answer.csv"
         hyper_questions[data_set] = {}
         df = pd.read_csv(data_set_path, header=None)
+        questions_count = len(df)
         for question, row in df.iterrows():
             for _, choice in row.items():
                 key = (data_set, question, choice)
@@ -22,20 +24,25 @@ def HyperMajorityVote(k):
                 majority_vote_all_answers[key] += 1
 
         hyper_questions_set_size = 3
-        while hyper_questions_set_size <=k:
+        while hyper_questions_set_size <= k:
             hyper_questions[data_set][hyper_questions_set_size] = {}
 
-            current_row = 0
-            sets_num = len(df) // hyper_questions_set_size + (1 if len(df) % hyper_questions_set_size != 0 else 0)
-            for i in range(sets_num):
-                current_question_range = range(current_row, min(current_row + hyper_questions_set_size, len(df)))
-                answers_to_hyper_questions = df.iloc[current_question_range][:].T
+            sets_num = questions_count * m // hyper_questions_set_size
+            seen_combinations = set()
+            for _ in range(sets_num):
+                current_question_set = sorted(random.sample(range(questions_count), k))
+                current_question_set_tuple = tuple(current_question_set)
+                if current_question_set_tuple in seen_combinations:
+                    continue
+                seen_combinations.add(current_question_set_tuple)
+                answers_to_hyper_questions = df.iloc[current_question_set][:].T
 
                 answers_set = answers_to_hyper_questions.value_counts().to_dict()
-                hyper_questions[data_set][hyper_questions_set_size][current_question_range] = answers_set
-                current_row += hyper_questions_set_size
-
+                hyper_questions[data_set][hyper_questions_set_size][
+                    current_question_set_tuple
+                ] = answers_set
             hyper_questions_set_size += 1
+        print(hyper_questions)
 
     hyper_majority_vote = {}
     questions = {}
@@ -63,11 +70,11 @@ def HyperMajorityVote(k):
     print("Normal MV:")
     print(majority_vote)
 
-    print("Hyper-MV: per K ",k)
+    print("Hyper-MV: per K ", k)
     print(hyper_majority_vote)
 
     print("Hyper-MV:per Question ")
     print(questions)
 
 
-HyperMajorityVote(4)
+HyperMajorityVote(4, 6)
