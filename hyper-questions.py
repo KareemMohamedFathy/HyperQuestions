088309464e7c26f026e5aee65e2e4cd011f23784
kareem fathy <kareem.abdelrahman@stud.tu-darmstadt.de>
2026-05-20 11:30:42 +0200

    feat: adding majority vote for both

--- hyper-questions.py
+++ hyper-questions.py
@@ -3,26 +3,71 @@ import pandas as pd
 from pathlib import Path
 
 data_sets = ['Chinese', 'English', 'IT', 'Medicine', 'Pokemon', 'Science']
-hyper_questions = {}
-for data_set in data_sets:
-    data_set_path = Path("datasets") / data_set /'answer.csv'
-    hyper_questions[data_set] = {}
-    df = pd.read_csv(data_set_path, header= None)
-    hyper_questions_set_size = 3
-    print(data_set)
-    while hyper_questions_set_size < 7:
-        hyper_questions[data_set][hyper_questions_set_size] = {} 
 
-        print(hyper_questions_set_size)
-        current_row = 0
-        sets_num = len(df) // hyper_questions_set_size + (1 if  len(df) % hyper_questions_set_size != 0 else 0)
-        for i in range(sets_num):
-            current_question_range = range(current_row, min(current_row+ hyper_questions_set_size, len(df)))
-            answers_to_hyper_questions = df.iloc[current_question_range][:].T
-            answers_set = answers_to_hyper_questions.value_counts().to_dict()
-            hyper_questions[data_set][hyper_questions_set_size][current_question_range] = answers_set
-            current_row += hyper_questions_set_size
-        hyper_questions_set_size += 1
 
-print(hyper_questions)
-            
+def HyperMajorityVote(k):
+    hyper_questions = {}
+    majority_vote_all_answers = {}
+    majority_vote = {}
+
+    for data_set in data_sets:
+        data_set_path = Path("datasets") / data_set / 'answer.csv'
+        hyper_questions[data_set] = {}
+        df = pd.read_csv(data_set_path, header=None)
+        for question, row in df.iterrows():
+            for _, choice in row.items():
+                key = (data_set, question, choice)
+                if key not in majority_vote_all_answers:
+                    majority_vote_all_answers[key] = 0
+                majority_vote_all_answers[key] += 1
+
+        hyper_questions_set_size = 3
+        while hyper_questions_set_size <=k:
+            hyper_questions[data_set][hyper_questions_set_size] = {}
+
+            current_row = 0
+            sets_num = len(df) // hyper_questions_set_size + (1 if len(df) % hyper_questions_set_size != 0 else 0)
+            for i in range(sets_num):
+                current_question_range = range(current_row, min(current_row + hyper_questions_set_size, len(df)))
+                answers_to_hyper_questions = df.iloc[current_question_range][:].T
+
+                answers_set = answers_to_hyper_questions.value_counts().to_dict()
+                hyper_questions[data_set][hyper_questions_set_size][current_question_range] = answers_set
+                current_row += hyper_questions_set_size
+
+            hyper_questions_set_size += 1
+
+    hyper_majority_vote = {}
+    questions = {}
+    for data_set, set_sizes in hyper_questions.items():
+        for set_size, question_ranges in set_sizes.items():
+            for question_range, answers_set in question_ranges.items():
+                answer_tuple, count = next(iter(answers_set.items()))
+                hyper_majority_vote[(data_set, question_range, answer_tuple)] = count
+
+    for (data_set, question_range, answer_tuple), count in hyper_majority_vote.items():
+        range_start = question_range.start
+        for index, answer in enumerate(answer_tuple):
+            question_number = range_start + index
+            questions[(data_set, question_number, answer)] = count
+
+    for (data_set, question, answer), frequency in majority_vote_all_answers.items():
+        key = (data_set, question)
+        if key not in majority_vote:
+            majority_vote[key] = (answer, frequency)
+        else:
+            current_ans, current_freq = majority_vote[key]
+            if frequency > current_freq:
+                majority_vote[key] = (answer, frequency)
+
+    print("Normal MV:")
+    print(majority_vote)
+
+    print("Hyper-MV: per K ",k)
+    print(hyper_majority_vote)
+
+    print("Hyper-MV:per Question ")
+    print(questions)
+
+
+HyperMajorityVote(4)
