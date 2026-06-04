04c915d7909b846065285afac5110732e267008d
kareem fathy <kareem.abdelrahman@stud.tu-darmstadt.de>
2026-06-04 15:44:51 +0200

    fix: removed extra file

--- hyper_questions.py
+++ hyper_questions.py
@@ -11,7 +11,7 @@ def HyperMajorityVote(k, m):
     hyper_questions = {}
     majority_vote_all_answers = {}
     majority_vote = {}
-
+    #Loading DataSet
     for data_set in data_sets:
         data_set_path = Path("datasets") / data_set / "answer.csv"
         hyper_questions[data_set] = {}
@@ -28,8 +28,8 @@ def HyperMajorityVote(k, m):
         hyper_questions[data_set][hyper_questions_set_size] = {}
 
         seen_combinations = set()
-        
-        for _ in range(m):
+        #Loading Permutations
+        for _ in range(m): 
             shuffled_questions = list(range(questions_count))
             random.shuffle(shuffled_questions)
 
@@ -48,6 +48,7 @@ def HyperMajorityVote(k, m):
                 ] = answers_set
     hyper_majority_vote = {}  
     questions_hyper_majority_vote = {}
+    #Picking winning tuple for each hyper question
     for data_set, set_sizes in hyper_questions.items():
         for set_size, question_ranges in set_sizes.items():
             for question_range, answers_set in question_ranges.items():
@@ -56,14 +57,14 @@ def HyperMajorityVote(k, m):
                 if  key not in hyper_majority_vote:
                      hyper_majority_vote[key] = 0
                 hyper_majority_vote[key]+=1
-
+ #Votes for each Question               
     for (data_set, question_range, answer_tuple), count in hyper_majority_vote.items():
         for question_number, answer in zip(question_range, answer_tuple):   
             key=(data_set, question_number, answer)
             if  key not in questions_hyper_majority_vote:
                 questions_hyper_majority_vote[key] = 0
             questions_hyper_majority_vote[key]+=1
-
+#Final hyperQuestions Majority vote
     final_hyper_mv = {}       
     for (data_set, question, answer), frequency in questions_hyper_majority_vote.items():
         key = (data_set, question)
@@ -74,8 +75,8 @@ def HyperMajorityVote(k, m):
             if frequency > current_freq:
                 final_hyper_mv[key] = (answer, frequency)
 
-
-    for (data_set, question, answer), frequency in majority_vote_all_answers.items():
+#Normal Majority Vote
+    for (data_set, question, answer), frequency in majority_vote_all_answers.items(): 
         key = (data_set, question)
         if key not in majority_vote:
             majority_vote[key] = (answer, frequency)
@@ -93,4 +94,4 @@ def HyperMajorityVote(k, m):
 
 
 if __name__ == "__main__":
-    HyperMajorityVote(4, 6)
+    HyperMajorityVote(4, 30)
